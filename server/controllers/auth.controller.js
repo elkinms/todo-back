7f96168959cd1a14101ebbd4ab5cdcfdc1b85cae
Mikhail <elkinms85@gmail.com>
2025-07-19 16:07:36 +0300

    fix bag in auth.controller user import

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecret'; // Лучше вынести в .env
 
